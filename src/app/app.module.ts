0ed7b0a19966d7dbd25dae5d2d168a96e8f5d012
Sebastian Alonso Gomez Arias <s.gomez@CO-IT00719.local>
2018-06-05 12:55:56 -0500

    Creando el provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { firebaseConfig } from '../environment';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TodosProvider } from '../providers/todos/todos';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TodosProvider
   ]
 })
 export class AppModule {}
