aa8a8fdda1a1b94bbd57a8311f7501fba9f0a90e
Sebastian Alonso Gomez Arias <s.gomez@CO-IT00719.local>
2018-06-05 12:59:52 -0500

    Filling the todos provider

--- src/providers/todos/todos.ts
+++ src/providers/todos/todos.ts
@@ -1,17 +1,40 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import firebase from 'firebase';
 
-/*
-  Generated class for the TodosProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
+
 export class TodosProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public db: AngularFirestore) {
     console.log('Hello TodosProvider Provider');
+  }
+
+  list() {
+    return this.db.collection('/todos', ref => ref.orderBy('complete').orderBy('text')).valueChanges();
+  }
+
+  add(text) {
+    const id = this.db.createId();
+    return this.db.collection('todos').doc(id).set({
+      id: id,
+      text: text,
+      complete: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+    });
+
+  }
+
+  complete(todo) {
+    return this.db.collection('todos').doc(todo.id).update({
+      complete: todo.complete,
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+    });
+  }
+
+  delete(todo) {
+    return this.db.collection('todos').doc(todo.id).delete();
   }
 
 }
