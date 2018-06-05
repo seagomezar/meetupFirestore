0a3836bb6869b97158401ced09c72abf03069dd9
Sebastian Alonso Gomez Arias <s.gomez@CO-IT00719.local>
2018-06-05 12:49:21 -0500

    Installing what we need and adding the environment file

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD7Iqk42XIVvNSO5N4FyrVKuuWmpTIzTC4",
+    authDomain: "sebastian-gomez-blog.firebaseapp.com",
+    databaseURL: "https://sebastian-gomez-blog.firebaseio.com",
+    projectId: "sebastian-gomez-blog",
+    storageBucket: "sebastian-gomez-blog.appspot.com",
+    messagingSenderId: "65616107590"
+};
