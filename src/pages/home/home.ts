2b370eca1feda38270679e082bdb26be12f87d21
Sebastian Alonso Gomez Arias <s.gomez@CO-IT00719.local>
2018-06-05 13:28:22 -0500

    Creating the component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TodosProvider } from '../../providers/todos/todos';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [TodosProvider]
 })
 export class HomePage {
+  public todos: Observable<any>;
+  public todo: String = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public todosProvider: TodosProvider) {
+    this.todos = this.todosProvider.list();
+  }
 
+  addTodo() {
+    if (!this.todo) {
+      return;
+    }
+
+    this.todosProvider.add(this.todo);
+    this.todo = '';
+  }
+
+  completeTodo(todo) {
+    this.todosProvider.complete(todo);
+  }
+  
+  deleteTodo(todo) {
+    this.todosProvider.delete(todo);
   }
 
 }
